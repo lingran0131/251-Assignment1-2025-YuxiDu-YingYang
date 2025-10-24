e8c12771d3c8cd17bed1b4c366045ebbfcee1cc3
lingran0131 <516651423@qq.com>
2025-10-24 17:22:56 +0800

    Add source code: main class, tests, and basic project structure

---
+++ src/main/java/ac/massey/TextEditor.java
@@ -0,0 +1,13 @@
+﻿package ac.massey;
+
+public class TextEditor {
+    public static void main(String[] args) {
+        System.out.println(\"=========================================\");
+        System.out.println(\"   文本编辑器 - 159.251 作业1\");
+        System.out.println(\"=========================================\");
+        System.out.println(\"作者: 杜雨熙 (24009219) 和 杨颖 (24009466)\");
+        System.out.println(\"项目基础设施配置完成 ✓\");
+        System.out.println(\"GUI功能开发中...\");
+        System.out.println(\"=========================================\");
+    }
+}
